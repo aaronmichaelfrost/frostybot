0989b3f94bef6d84c95ecd44bf049a6438140ece
AaronFrostDev-atch <aaronmichaelfrost@gmail.com>
2020-12-20 14:23:35 -0800

    BehaviorController + SteerTo algorithm

--- CSharpBot/Bot/Bot.cs
+++ CSharpBot/Bot/Bot.cs
@@ -5,6 +5,9 @@ using Bot.Utilities.Processed.FieldInfo;
 using Bot.Utilities.Processed.Packet;
 using RLBotDotNet;
 
+using System;
+using System.Diagnostics;
+
 using System.Collections.Generic;
 
 
@@ -12,10 +15,108 @@ using System.Collections.Generic;
 
 
 
+
 namespace Bot
 {
 
-    
+
+    public class BehaviorController
+    {
+        public Controller controller;
+        private FrostyBot agent;
+        private List<Behavior> behaviors = new List<Behavior>();
+        private Behavior defaultBehavior = new TakeShot();
+        public Behavior currentBehavior;
+
+        public BehaviorController(ref FrostyBot agent) 
+        { 
+            this.agent = agent;
+
+            // Add behaviors to list in order of priority
+            behaviors.Add(new TakeShot());
+            behaviors.Add(new GetBoost());
+        }
+
+        private Behavior SelectBehavior()
+        {
+            if(behaviors.Count > 0)
+                for (int i = 0; i < behaviors.Count; i++)
+                    if (behaviors[i].IsViable(agent))
+                    {
+                        ResetController();
+                        return behaviors[i];
+                    }
+                        
+
+            return defaultBehavior;
+        }
+
+        public void Behave()
+        {
+            currentBehavior = SelectBehavior();
+            currentBehavior.Behave(ref agent);
+        }
+
+        public void ResetController()
+        {
+            agent.controller = new Controller();
+        }
+    }
+
+
+    public abstract class Behavior
+    {
+        public abstract string Name();
+        public abstract bool IsViable(FrostyBot agent);
+        public abstract void Behave(ref FrostyBot agent);
+        
+    }
+
+
+    public class TakeShot : Behavior
+    {
+        public override string Name() { return "SetupShot"; }
+        public override bool IsViable(FrostyBot agent)
+        {
+            return agent.boostAmount > 40;
+        }
+        public override void Behave(ref FrostyBot agent)
+        {
+            //agent.ApproachTarget(agent.setupPosition, false, 1);
+            agent.controller.Throttle = 1f;
+            agent.SteerTo(agent.field.ballLocation);
+            if (Vector3.Distance(agent.transform.location, agent.field.ballLocation) < 1000 && agent.field.ballLocation.Y - 100 < agent.transform.location.Y)
+            {
+
+                agent.controller.Boost = true;
+  
+            }
+            else
+            {
+                agent.controller.Boost = false;
+            }
+        }
+    }
+
+
+    public class GetBoost : Behavior
+    {
+        public override string Name() { return "GetBoost"; }
+        public override bool IsViable(FrostyBot agent)
+        {
+            if (agent.boostAmount < 80)
+                return true;
+            return false;
+        }
+        public override void Behave(ref FrostyBot agent)
+        {
+            agent.controller.Throttle = 1;
+            agent.controller.Boost = true;
+            agent.SteerTo(agent.NearestBoost().location);
+        }
+    }
+
+
     public class Boost
     {
         public Vector3 location;
@@ -35,53 +136,193 @@ namespace Bot
     }
 
 
-    // We want to our bot to derive from Bot, and then implement its abstract methods.
-    class FrostyBot : RLBotDotNet.Bot
+    public struct Opponent
+    {
+        public Transform transform;
+        public int index;
+    }
+
+
+    public struct Field
+    {
+        public List<Boost> boosts;
+        public Vector3 ballLocation;
+        public Vector3 ballRelativeLocation;
+        public Vector3 ballVelocity;
+        public BallPrediction ballPrediction;
+    }
+
+
+    public struct Transform
+    {
+        public Vector3 location;
+        public Orientation rotation;
+    }
+
+
+    public class FrostyBot : RLBotDotNet.Bot
     {
 
-        List<Boost> boosts = new List<Boost>();
-        private int opponentIndex;
+        public Field field;
+        public Opponent opponent;
+        public Transform transform;
+        public Controller controller;
+        public int boostAmount;
+        public Packet packet;
+
+        public float forwardsVelocity;
+
+        public bool ballInLineWithEnemyGoal;
+        public Vector3 setupPosition;
+        
+
+        private BehaviorController behaviorController;
 
 
+        Vector2 origin = new Vector2();
+        Vector2 midpoint = new Vector2();
+        Vector2 position2D = new Vector2();
 
-        // We want the constructor for our Bot to extend from RLBotDotNet.Bot, but we don't want to add anything to it.
-        // You might want to add logging initialisation or other types of setup up here before the bot starts.
-        public FrostyBot(string botName, int botTeam, int botIndex) : base(botName, botTeam, botIndex) {
-            SetOpponentIndex();
-            InitializeBoostLocations();
+        public void SteerTo(Vector3 targetLocation)
+        {
+            // Set steer value so that we will travel along the shortest circle that reaches the destination
+
+            float slopeTangent = transform.rotation.Forward.Y / transform.rotation.Forward.X;
+
+            float slopePerpendicular = -1 / slopeTangent;
+            float interceptPerpedicular = transform.location.Y + transform.location.X / slopeTangent;
+
+            midpoint.X = (transform.location.X + targetLocation.X) / 2;
+            midpoint.Y = (transform.location.Y + targetLocation.Y) / 2;
+
+            float slopeChord = (transform.location.Y - targetLocation.Y) / (transform.location.X - targetLocation.X);
+
+            float slopePerpendicularBisector = -1 / slopeChord;
+            float interceptPerpendicularBisector = midpoint.Y + midpoint.X / slopeChord;
+
+            origin.X = (interceptPerpendicularBisector - interceptPerpedicular) / (slopePerpendicular - slopePerpendicularBisector);
+            origin.Y = slopePerpendicular * origin.X + interceptPerpedicular;
+
+            position2D.X = transform.location.X;
+            position2D.Y = transform.location.Y;
+
+            float circleRadius = Vector2.Distance(origin, position2D);
+
+            Renderer.DrawCenteredRectangle3D(Colors.Blue, new Vector3(origin.X, origin.Y, 10), 20, 20, true);
+
+            float steer = (float)TurnRadius(forwardsVelocity) / circleRadius;
+
+            if (steer > 1)
+                steer = 1;
+            if (steer < -1)
+                steer = -1;
+
+            if (field.ballRelativeLocation.Y < 0)
+                steer *= -1;
+
+            controller.Steer = steer;
+
+            // Powerslide if needed
+            if (Vector3.Dot(forwardsDirection, targetLocation - transform.location) < .5)
+                controller.Handbrake = true;
+            else
+                controller.Handbrake = false;
         }
 
 
-        void InitializeBoostLocations()
+
+        public FrostyBot(string botName, int botTeam, int botIndex) : base(botName, botTeam, botIndex) {
+
+            Init();
+            FrostyBot me = this;
+            behaviorController = new BehaviorController(ref me);
+
+        }
+
+        public bool BallInLineWithEnemyGoal()
         {
-            int n = GetFieldInfo().BoostPads.Length;
+            float m = (transform.location.Y - field.ballLocation.Y) / (transform.location.X - field.ballLocation.X);
+
+            float b = transform.location.Y - m * transform.location.X;
+
+            float intersectX;
+
+            if (team == 0)
+            {
+                intersectX = (-5120 - b) / m;
+
+                if (field.ballLocation.Y < transform.location.Y)
+                    return false;
+            }
+            else
+            {
+                intersectX = (5120 - b) / m;
+
+                if (field.ballLocation.Y > transform.location.Y)
+                    return false;
+            }
+
+            
+
+            if (intersectX > -800.25 && intersectX < 800.25)
+                return true;
+
+                
+
+            return false;
+        }
+
+        double Curvature(float forwardVelocity)
+        {
+            if (0.0 <= forwardVelocity && forwardVelocity < 500.0)
+                return 0.006900 - 5.84e-6 * forwardVelocity;
+            if (500.0 <= forwardVelocity && forwardVelocity < 1000.0)
+                return 0.005610 - 3.26e-6 * forwardVelocity;
+            if (1000.0 <= forwardVelocity && forwardVelocity < 1500.0)
+                return 0.004300 - 1.95e-6 * forwardVelocity;
+            if (1500.0 <= forwardVelocity && forwardVelocity < 1750.0)
+                return 0.003025 - 1.1e-6 * forwardVelocity;
+            if (1750.0 <= forwardVelocity && forwardVelocity < 2500.0)
+                return 0.001800 - 4e-7 * forwardVelocity;
+
+            return 0.0;
+        }
+
+        double TurnRadius(float forwardVelocity)
+        {
+            if (forwardVelocity == 0)
+                return 0;
+            return 1.0 / Curvature(forwardVelocity);
+        }
+
+
+        void Init()
+        {
+            opponent.index = (index == 1) ? 0 : 1;
+
+            field.boosts = new List<Boost>();
+            FieldInfo fieldInfo = GetFieldInfo();
+            int n = fieldInfo.BoostPads.Length;
             for (int i = 0; i < n; i++)
             {
-                BoostPad b = GetFieldInfo().BoostPads[i];
-                boosts.Add(new Boost(b.Location, b.IsFullBoost, i));
+                BoostPad b = fieldInfo.BoostPads[i];
+                field.boosts.Add(new Boost(b.Location, b.IsFullBoost, i));
             }
         }
 
 
-        private void SetOpponentIndex()
-        {
-            opponentIndex = index == 1 ? 0 : 1;
-        }
-
-
-        Boost IdealFullBoostPad(Packet gameTickPacket, Vector3 carLocation)
+        // Return the closest full and active boost pad to the player
+        public Boost NearestBoost()
         {
 
-            // Return the closest full and active boost pad to the player
-
-            Boost bestOption = boosts[0];
+            Boost bestOption = field.boosts[0];
 
             for (int i = 0; i < GetFieldInfo().BoostPads.Length; i++)
             {
 
-                Boost currentPad = boosts[i];
+                Boost currentPad = field.boosts[i];
 
-                if ((currentPad.isFull && boosts[i].isActive && Vector3.Distance(currentPad.location, carLocation) < Vector3.Distance(bestOption.location, carLocation)) 
+                if ((currentPad.isFull && field.boosts[i].isActive && Vector3.Distance(currentPad.location, transform.location) < Vector3.Distance(bestOption.location, transform.location)) 
                     || (!bestOption.isFull || !bestOption.isActive))
                     bestOption = currentPad;
             }
@@ -93,90 +334,79 @@ namespace Bot
         }
 
 
-        void UpdateBoostStates(Packet packet)
+        public Vector3 forwardsDirection = new Vector3();
+
+        // Update the bot's vision
+        void CollectInfo()
         {
-            for (int i = 0; i < boosts.Count; i++)
+            for (int i = 0; i < field.boosts.Count; i++)
             {
-                boosts[i].isActive = packet.BoostPadStates[i].IsActive;
-                boosts[i].timer = packet.BoostPadStates[i].Timer;
+                field.boosts[i].isActive = packet.BoostPadStates[i].IsActive;
+                field.boosts[i].timer = packet.BoostPadStates[i].Timer;
             }
+
+            transform.location = packet.Players[index].Physics.Location;
+            transform.rotation = packet.Players[index].Physics.Rotation;
+
+
+            double x = Math.Cos(transform.rotation.Pitch) * Math.Cos(transform.rotation.Yaw);
+            double y = Math.Cos(transform.rotation.Pitch) * Math.Sin(transform.rotation.Yaw);
+            double z = Math.Sin(transform.rotation.Pitch);
+
+
+
+            forwardsDirection = new Vector3((float)x, (float)y, (float)z);
+            forwardsVelocity = Vector3.Dot(forwardsDirection, packet.Players[index].Physics.Velocity);
+            Renderer.DrawString2D("FVel " + forwardsVelocity, Colors.Red, currentStateTextPos + new Vector2(0, 50), 2, 2);
+
+            boostAmount = packet.Players[index].Boost;
+
+            //opponent.transform.location = packet.Players[opponent.index].Physics.Location;
+            //opponent.transform.rotation = packet.Players[opponent.index].Physics.Rotation;
+
+            field.ballLocation = packet.Ball.Physics.Location;
+            field.ballRelativeLocation = Orientation.RelativeLocation(transform.location, field.ballLocation, transform.rotation);
+            field.ballVelocity = packet.Ball.Physics.Velocity;
+
+            field.ballPrediction = GetBallPrediction();
+
         }
 
-            
-
-
-            
-        
-        
-
-        
 
         public override Controller GetOutput(rlbot.flat.GameTickPacket gameTickPacket)
         {
-            // We process the gameTickPacket and convert it to our own internal data structure.
-            Packet packet = new Packet(gameTickPacket);
+            packet = new Packet(gameTickPacket);
 
+            CollectInfo();
 
-            UpdateBoostStates(packet);
+            behaviorController.Behave();
 
-            // Get prediction for ball 25 game ticks in the future
-            //GetBallPrediction().Slices[25].Physics.Location;
+            Render();
 
-
-            Vector3 ballLocation = packet.Ball.Physics.Location;
-            Vector3 ballVelocity = packet.Ball.Physics.Velocity;
-
-
-            Vector3 carLocation = packet.Players[index].Physics.Location;
-            Orientation carRotation = packet.Players[index].Physics.Rotation;
-
-
-            Vector3 opponentLocation = packet.Players[opponentIndex].Physics.Location;
-            Orientation opponentRotation = packet.Players[opponentIndex].Physics.Rotation;
-
-
-            // Find where the ball is relative to us.
-            Vector3 ballRelativeLocation = Orientation.RelativeLocation(carLocation, ballLocation, carRotation);
-
-            Vector3 idealFullBoostRelativeLocation = Orientation.RelativeLocation(carLocation, IdealFullBoostPad(packet, carLocation).location, carRotation);
-
-
-            // Decide which way to steer in order to get to the ball.
-            // If the ball is to our left, we steer left. Otherwise we steer right.
-            float steer;
-            if (idealFullBoostRelativeLocation.Y > 0)
-                steer = 1;
-            else
-                steer = -1;
-
-
-
-
-            // Get boost
-            //Vector3 nearestBoostLocation = 
-            
-
-
-
-            // Examples of rendering in the game
-            Renderer.DrawString3D("Ball", Colors.Black, ballLocation, 3, 3);
-            Renderer.DrawString3D(steer > 0 ? "Right" : "Left", Colors.Aqua, carLocation, 3, 3);
-            Renderer.DrawLine3D(Colors.Red, carLocation, ballLocation);
-
-            Renderer.DrawLine3D(Colors.Red, carLocation, opponentLocation);
-
-            // This controller will contain all the inputs that we want the bot to perform.
-            return new Controller
-            {
-                // Set the throttle to 1 so the bot can move.
-                Throttle = 1,
-                Steer = steer,
-
-
-                Jump = true
-
-            };
+            return controller;
         }
+
+
+
+
+        Vector2 currentStateTextPos = new Vector2(0, 0);
+
+
+        private void Render()
+        {
+            Renderer.DrawString3D("Ball", Colors.Black, field.ballLocation, 3, 3);
+            Renderer.DrawString3D(controller.Steer > 0 ? "Right" : "Left", Colors.Aqua, transform.location, 3, 3);
+            Renderer.DrawLine3D(Colors.Red, transform.location, field.ballLocation);
+            Renderer.DrawLine3D(Colors.Red, transform.location, opponent.transform.location);
+
+
+            Renderer.DrawString2D("Steer " + controller.Steer, Colors.Red, currentStateTextPos, 2, 2);
+
+            //Renderer.DrawString2D("Current State: " + behaviorController.currentBehavior.Name(), Colors.Red, currentStateTextPos, 2, 2);
+        }
+
+
+
         // Hide the old methods that return Flatbuffers objects and use our own methods that
         // use processed versions of those objects instead.
         internal new FieldInfo GetFieldInfo() => new FieldInfo(base.GetFieldInfo());
